3fbdec1afb3d43ea58d0237092b81ee96c231cea
Jerry John Comellas <jerry.comellas@gmail.com>
2015-05-20 00:16:47 -0400

    Updated Week 3 Assignment which is now called resumeProfessional and includes Sass

---
+++ scripts/scripts.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+
+    //Contact Me Button//
+    // $("#button1").click(function() {
+    //     document.getElementById("button1").innerHTML = ("Thank you for contacting me.");
+    //     console.log("Contact me button was clicked. Check your email.");
+    // });
+
+
+    //CV Download Button//
+    $("#button2").click(function() {
+        document.getElementById("button2").innerHTML = ("");
+        console.log("CV pdf was downloaded.");
+    });
+
+
+    //countTo//
+    $('.timer').countTo();
+
+
+    //Smooth Scroll//
+    smoothScroll.init();
+
+
+    //Owl Carousel//
+    $("#owl-example").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+        });
+
+
+    //Wow//
+    new WOW().init();
+
+
+    //Simple Text Rotator//
+    $(".rotate").textrotator({
+        animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+        speed: 9000 // How many milliseconds until the next word show.
+    });
+
+
+    //Skillbar//
+    jQuery(document).ready(function() {
+        jQuery('.skillbar').each(function() {
+            jQuery(this).find('.skillbar-bar').animate({
+                width: jQuery(this).attr('data-percent')
+            }, 6000);
+        });
+    });
+
+
+
+});
